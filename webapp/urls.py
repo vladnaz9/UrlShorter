0c005eb44d768c65e2fd2053362b771e04970837
Nazarychev_905 <vladnaz9@mail.ru>
2022-05-15 12:16:39 +0300

    add view

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Главная страница
     path('', views.index),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# Главная страница
+def index(request):
+    return render(request, 'webapp/index.html')
+
+
