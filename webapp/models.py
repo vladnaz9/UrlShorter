61428a2fac8af5ca769ea7f487fe648889b89aa7
Nazarychev_905 <vladnaz9@mail.ru>
2022-05-15 13:42:54 +0300

    add model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import os
+from typing import Callable
+from dataclasses import dataclass
+import base64
+import hashlib
+import hmac
 
 # Create your models here.
+# Модель ссылки
+class UrlShorter(models.Model):
+    created = models.DateTimeField(auto_now_add=True)  # дата создания
+    long_url = models.URLField()  # полная ссылка
+    short_url = models.URLField(max_length=10, unique=True, blank=True)  # короткая ссылка
+    clicks = models.PositiveIntegerField(default=0)  # сколько раз использовали ссылку
+
+
+    class Meta:
+        ordering = ["-clicks"]
+
+    def __str__(self):
+        return f'{self.long_url} to {self.short_url}'
+
+    def clicked(self):
+        self.clicks += 1
+        self.save()
+
